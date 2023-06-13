9a9b4d34765ae260ae2aba2bf1487676c60e2001
azade95 <absalamzadeazade95@gmail.com>
2023-06-13 18:39:21 +0400

    Add project files.

---
+++ YatriiWorld/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using YatriiWorld.Models;
+using YatriiWorld.Utilities.Extensions;
+using YatriiWorld.ViewModels;
+
+namespace YatriiWorld.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM newuser)
+        {
+            if (!ModelState.IsValid) return View();
+
+            if (!newuser.Email.CheckEmail())
+            {
+                ModelState.AddModelError("Email", "Email formati dogru deyil");
+                return View();
+            }
+            AppUser user = new AppUser
+            {
+                Name = newuser.Name.Capitalize(),
+                Surname = newuser.Surname.Capitalize(),
+                Email = newuser.Email,
+                UserName = newuser.Name,
+                Gender = newuser.Gender,
+
+            };
+            IdentityResult result = await _userManager.CreateAsync(user, newuser.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View();
+            }
+            await _signInManager.SignInAsync(user, false);
+
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser existed = await _userManager.FindByNameAsync(loginVM.UsernameOrEmail);
+            if (existed == null)
+            {
+                existed = await _userManager.FindByEmailAsync(loginVM.UsernameOrEmail);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(existed, loginVM.Password, loginVM.IsRemember, true);
+            if (!result.Succeeded)
+            {
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ YatriiWorld/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using YatriiWorld.DAL;
+using YatriiWorld.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredUniqueChars = 1;
+
+    options.User.RequireUniqueEmail = true;
+
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.AllowedForNewUsers = true;
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute("Areas", "{area:exists}/{controller=home}/{action=index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
